0ded341eccf4ed234f4770ce7341fd408501d57e
Waleed0248 <Waleed0248>
2024-10-23 09:29:48 +0500

    updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-      domains: ['cdn.weatherapi.com'], // Allow images from this domain
+      domains: ['cdn.weatherapi.com'],
     },
   };
   
-  export default nextConfig; // Use export default instead of module.exports
+  export default nextConfig; 
   
   
